53e589d4ef909e92e450ab50312669251f7157f2
RCorsino <corsinoronalyn@gmail.com>
2015-10-28 09:41:56 +0800

    Validator for the authentication of user.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -49,13 +49,11 @@ class AuthController extends Controller
      */
     protected function validator(array $data)
     {
-        return Validator::make($data, [
-            'something' => 'required',
+        return Validator::make($data, [         
             'name' => 'required|max:255|unique:users',
+            'email' => 'required|email|max:255|unique:users',
             'password' => 'required|confirmed|min:6',
         ]);
-
-        // 'email' => 'required|email|max:255|unique:users',
     }
 
      public function loginUsername()
